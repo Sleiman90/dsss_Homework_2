8e44d20c7f2467590ed292d7d42742e58f6d322c
sleiman <sleimansharara@gmail.com>
2023-11-11 18:12:29 +0100

    commit setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 11 18:10:22 2023
+
+@author: HP
+"""
+
+from setuptools import setup, find_packages
+
+setup(
+    name='math_quiz',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        # Add any dependencies here
+    ],
+)
